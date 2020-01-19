5c05bf6fa717986e96f671e61c104768d1fd06ea
David Gantiva <dagantivac@unal.edu.co>
2020-01-19 14:28:52 -0500

    metrics

--- compile.sh
+++ compile.sh
@@ -2,3 +2,5 @@ clear
 mpic++ -o mpi_test mpiSecuencial.cpp
 #mpirun -np 1 -mca btl ^openib mpi_test
 time mpirun -np 1 --host localhost -mca btl ^openib mpi_test
+time mpirun -np 4 --host localhost,10.0.0.5,10.0.0.6,10.0.0.7 -mca btl ^openib mpi_test
+
