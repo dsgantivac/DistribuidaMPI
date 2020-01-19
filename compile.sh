735d5af7460c4b296e6bec88f4f53eadc82ed418
David Gantiva <dagantivac@unal.edu.co>
2020-01-19 14:33:28 -0500

    change compile

--- compile.sh
+++ compile.sh
@@ -2,5 +2,7 @@ clear
 mpic++ -o mpi_test mpiSecuencial.cpp
 #mpirun -np 1 -mca btl ^openib mpi_test
 time mpirun -np 1 --host localhost -mca btl ^openib mpi_test
+time mpirun -np 2 --host localhost,10.0.0.5 -mca btl ^openib mpi_test
+time mpirun -np 3 --host localhost,10.0.0.5,10.0.0.6 -mca btl ^openib mpi_test
 time mpirun -np 4 --host localhost,10.0.0.5,10.0.0.6,10.0.0.7 -mca btl ^openib mpi_test
 
