9628435f8fc1c509a813418ab88804295adb08cd
David Gantiva <dagantivac@unal.edu.co>
2020-01-19 12:58:07 -0500

    total frcuency

--- mpiSecuencial.cpp
+++ mpiSecuencial.cpp
@@ -257,6 +257,14 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
+		cout<<"Total frecuency"<<endl;
+		for(int i= 0; i< 34; i++){
+			for(int j=0;j<32;j++)
+				cout<< *(totalFrecuency +i*34+j)<<"-" <<endl;
+			cout<<endl;
+		}
+
+
 		for(int k=0;k<KCLUSTERS;k++){
 			for(int i =0;i<32;i++){
 				int aux = -1;
