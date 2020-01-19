a376c34c288f87d60c18b334305535dfd21f3ebc
David Gantiva <dagantivac@unal.edu.co>
2020-01-19 16:15:11 -0500

    adding

--- mpiSecuencial.cpp
+++ mpiSecuencial.cpp
@@ -16,7 +16,7 @@ VERSION SECUENCIAL YA FUNCIONAL
 #include <sstream> 
 #include <mpi.h>
 
-#define KCLUSTERS 125
+#define KCLUSTERS 50
 #define ITERATIONS 1
 int limitLoop = 6;
 int limit = 1000000;
